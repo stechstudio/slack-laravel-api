6d3b6345c42f12d2d3e0f732c6832f082baa7ba2
Bubba <rob@stechstudio.com>
2019-02-20 18:46:53 -0500

    Option Composition Object Implementation

--- src/Messaging/CompositionObjects/Option.php
+++ src/Messaging/CompositionObjects/Option.php
@@ -8,7 +8,57 @@
 
 namespace STS\Slack\Messaging\CompositionObjects;
 
+/**
+ * An object that represents a single selectable item in a select menu.
+ */
 class Option
 {
+    /**
+     * A plain_text only text object that defines the text shown in the option on the menu.
+     *
+     * @var Text
+     */
+    protected $text;
+    /**
+     * The string value that will be passed to your app when this option is chosen.
+     *
+     * @var string
+     */
+    protected $value;
 
+    public function __construct(Text $text, string $value)
+    {
+        $this->text = $text->setType('plain_text');
+        $this->value = $value;
+    }
+
+    public static function createFromString(string $text, string $value): self
+    {
+        return new static(
+            new Text($text),
+            $value
+        );
+    }
+
+    public function getText(): Text
+    {
+        return $this->text;
+    }
+
+    public function setText(Text $text): Option
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): Option
+    {
+        $this->value = $value;
+        return $this;
+    }
 }
