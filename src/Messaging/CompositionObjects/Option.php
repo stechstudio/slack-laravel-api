390e7657adc2f087fd4233e0726a441fc7298cc0
Bubba <rob@stechstudio.com>
2019-02-20 18:40:08 -0500

    Option composition objext stub

---
+++ src/Messaging/CompositionObjects/Option.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+/**
+ * Package: slack-sdk-php
+ * Create Date: 2019-02-20
+ * Created Time: 17:37
+ */
+
+namespace STS\Slack\Messaging\CompositionObjects;
+
+class Option
+{
+
+}
