d9bd732b8fd94f434588157d84722cd972bf8ecf
Bubba <rob@stechstudio.com>
2019-02-20 17:03:08 -0500

    An exception for when there is no handler registered

---
+++ src/Exceptions/HandlerUndefined.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+/**
+ * Package: slack-sdk-php
+ * Create Date: 2019-02-20
+ * Created Time: 15:42
+ */
+
+namespace STS\Slack\Exceptions;
+
+use InvalidArgumentException;
+
+class HandlerUndefined extends InvalidArgumentException
+{
+
+}
