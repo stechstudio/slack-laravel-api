100fbac790159781fa09f30c91c89f8768517ed9
Bubba <rob@stechstudio.com>
2019-02-20 18:41:00 -0500

    Context Layout Block stub

---
+++ src/Messaging/LayoutBlocks/Context.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+/**
+ * Package: slack-sdk-php
+ * Create Date: 2019-02-20
+ * Created Time: 17:35
+ */
+
+namespace STS\Slack\Messaging\LayoutBlocks;
+
+class Context
+{
+
+}
