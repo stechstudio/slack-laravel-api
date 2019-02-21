910fafe76c2f81d748c43e25e665a31cf0df76e0
Bubba <rob@stechstudio.com>
2019-02-20 19:31:07 -0500

    Stub out the Response object for now

---
+++ src/Http/Response.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+/**
+ * Package: slack-sdk-php
+ * Create Date: 2019-02-20
+ * Created Time: 17:16
+ */
+
+namespace STS\Slack\Http;
+
+use Illuminate\Http\Response as IlluminateResponse;
+
+class Response extends IlluminateResponse
+{
+
+    protected function generateMessagePayload(): array
+    {
+
+    }
+}
