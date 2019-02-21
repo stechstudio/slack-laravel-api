88d5d33a038b4cbaefc19a9f347a2528b5afd57d
Bubba <rob@stechstudio.com>
2019-02-20 19:14:30 -0500

    Image Block Element stub

---
+++ src/Messaging/BlockElements/Button.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+/**
+ * Package: slack-sdk-php
+ * Create Date: 2019-02-20
+ * Created Time: 19:13
+ */
+
+namespace STS\Slack\Messaging\BlockElements;
+
+class Button
+{
+
+}
